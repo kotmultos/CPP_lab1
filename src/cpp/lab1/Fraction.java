3df73f5e262f32a7f00220a0467f9b36b369d45f
Oleksandr Shpak <kotmultos@gmail.com>
2022-08-27 16:14:00 +0300

    init commit

---
+++ src/cpp/lab1/Fraction.java
@@ -0,0 +1,7 @@
+package cpp.lab1;
+
+public class Fraction {
+    private int nominator;
+    private int denominator;
+
+}
---
+++ src/cpp/lab1/Lab1.java
@@ -0,0 +1,16 @@
+package cpp.lab1;
+
+import java.util.Scanner;
+
+public class Lab1 {
+    public static void main(String [] args){
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the number n: ");
+        int n = scanner.nextInt();
+        System.out.println("You entered: " + n);
+        System.out.println("Your data is being processed now, please wait...");
+
+
+    }
+}
